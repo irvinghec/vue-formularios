7d0e6f1e5f8595c82a17279c0b367ca715f9c268
irvinghec <a3519210049@alumno.uttehuacan.edu.mx>
2022-10-17 15:36:36 -0600

    formularios2

---
+++ src/components/Formulario.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="row">
+    <div class="col-12 mb-4">
+      <progress-bar :porcentaje="porcentaje" />
+    </div>
+    <div class="col-12 col-md-4">
+      <form @submit.prevent="registrarProyecto">
+        <div class="mb-3">
+          <label class="form-label">Proyecto</label>
+          <input
+            v-model.trim="proyecto"
+            type="text"
+            class="form-control"
+            required
+          />
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Actividad</label>
+          <select v-model.trim="tipo" class="form-select" required>
+            <option disabled selected value="">
+              Selecciona un tipo de actividad
+            </option>
+            <option>Aplicaciones Web con Vue.js</option>
+            <option>Backend Services con Node</option>
+            <option>App móvil con React Native</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-check-label"
+            >Urgente</label
+          >
+          <input v-model="urgente" type="checkbox" class="form-checkbox" />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
+    </div>
+    <div class="col-12 col-md-8">
+      <total-proyectos
+        :numeroProyectos="numeroProyectos"
+        :proyectos="proyectos"
+        :cambiarEstado="cambiarEstado"
+        :limpiarData="limpiarData"
+        :borrarItem="borrarItem"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import ProgressBar from "./ProgressBar.vue";
+import TotalProyectos from "./TotalProyectos.vue";
+export default {
+  data: () => ({
+    proyecto: "",
+    tipo: "",
+    urgente: false,
+    proyectos: [],
+  }),
+  methods: {
+    registrarProyecto() {
+      const proyecto = {
+        proyecto: this.proyecto,
+        tipo: this.tipo,
+        urgente: this.urgente,
+        completado: false,
+      };
+      this.proyectos.push(proyecto);
+      this.saveData();
+
+      this.proyecto = "";
+      this.tipo = "";
+      this.urgente = false;
+    },
+    cambiarEstado(proyecto, campo) {
+      // this.proyectos[id].urgente = !this.proyectos[id].urgente;
+      proyecto[campo] = !proyecto[campo];
+      this.saveData();
+    },
+    saveData() {
+      localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
+    },
+    limpiarData() {
+      this.proyectos = [];
+      localStorage.clear();
+    },
+    borrarItem(proyecto) {
+      console.log(proyecto);
+      this.proyectos = this.proyectos.filter(
+        (proy) => proy.proyecto != proyecto.proyecto
+      );
+      this.saveData();
+    },
+  },
+  computed: {
+    numeroProyectos() {
+      return this.proyectos.length;
+    },
+    porcentaje() {
+      let completados = 0;
+      this.proyectos.map((proyecto) => {
+        if (proyecto.completado) completados++;
+      });
+      return (completados * 100) / this.numeroProyectos || 0;
+    },
+  },
+  components: { ProgressBar, TotalProyectos },
+  mounted() {
+    this.proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+  },
+};
+</script>
